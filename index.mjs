5760f88db24d1cfaa00db96c1ca68f1dc7461665
Tiril Mageli <tiril.mageli23@imperial.ac.uk>
2024-02-21 13:15:15 +0000

    Added monday filtering

--- index.mjs
+++ index.mjs
@@ -1,7 +1,8 @@
 import AWS from 'aws-sdk';
 import jwt from 'jsonwebtoken';
 
-AWS.config.update({ region: 'eu-west-2' });
+region = 'eu-west-2'
+AWS.config.update({ region: region });
 const dynamo_db_client = DynamoDBDocumentClient.from(new DynamoDB({ region }));
 const ddb = new AWS.DynamoDB.DocumentClient();
 
@@ -13,12 +14,20 @@ export const handler = async (event) => {
       const decoded = jwt.decode(token);
       const user_id = decoded.sub;
       console.log("Decoded JWT user ID:", user_id);
+
+      // Get seconds since last monday
+      const now = new Date();
+      let dayOfWeek = now.getDay(); // 0 = Sunday; 1 = Monday etc.
+      let daysSinceLastMonday = (dayOfWeek + 6) % 7
+      let mondayDate = new Date(now.getTime() - daysSinceLastMonday * 24 * 60 * 60 * 1000)
+      mondayDate.setHours(0, 0, 0, 0); // Set time to 00:00:00
+      let mondayTime = Math.floor(mondayDate.getTime() / 1000) // Convert back to seconds
   
       // Construct the DynamoDB parameters
       const params = {
         TableName: 'trainings_log',
-        FilterExpression: 'user_id = :user_id',
-        ExpressionAttributeValues: { ':user_id': user_id }
+        KeyConditionExpression: 'user_id = :user_id and timestamp_local >= :monday_time',
+        ExpressionAttributeValues: { ':user_id': user_id, 'monday_time': mondayTime.toString() }
       };
   
       // Perform a scan operation to retrieve the items from the table
